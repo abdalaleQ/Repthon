824fce74aa52bc3e5faad122cb4b61549f464402
abdalaleQ <153198040+abdalaleQ@users.noreply.github.com>
2023-12-27 22:51:05 +0300

    Update ايدي.py

--- repthon/plugins/ايدي.py
+++ repthon/plugins/ايدي.py
@@ -87,7 +87,7 @@ async def fetch_info(replied_user, event):
     caption += f"<b> {REP_EM}╎الحساب ⇠ </b> "
     caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
     caption += f"\n<b> {REP_EM}╎البايـو    ⇠ </b> {user_bio} \n"
-    caption += f"✛━━━━الزعيم━━ @T_5_G ━━المطور━━━━━✛"
+    caption += f"✛━━ @T_5_G ━المطور━✛"
     return photo, caption
 
 @zq_lo.rep_cmd(
